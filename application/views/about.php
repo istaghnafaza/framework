c53e30039ff1e406bf8ca20c0eede003c102b872
istaghnafaza <istaghnafaza@gmail.com>
2017-03-06 19:56:27 +0700

    Faza

---
+++ application/views/about.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Istaghna Faza</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+	<div class="container-fluid">
+		<nav class="navbar navbar-default" role="navigation">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="<?php site_url('home') ?>">HOME</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<ul class="nav navbar-nav">
+				<li ><a href="<?php echo site_url('About'); ?>">About</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+			
+			
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div>
+</nav>
+<div class="container">
+	<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Biography</h3>
+	</div>
+	<div class="panel-body">
+	<table class="table table-hover">
+		<tbody>
+			<tr>
+				<th>Name</th>
+				<th>Istaghna Faza Kamil</th>
+			</tr>
+			<tr>
+				<th>NIM</th>
+				<th>1541180083</th>
+			</tr>
+			<tr>
+				<th>Born</th>
+				<th>Malang, 18 June 1997</th>
+			</tr>
+			<tr>
+				<th>Religion</th>
+				<th>Moslem</th>
+			</tr>
+			<tr>
+				<th>Address</th>
+				<th>Bandara Juanda I-AA.17</th>
+			</tr>
+			<tr>
+				<th>Contact</th>
+				<th>082231220312</th>
+			</tr>
+			<tr>
+				<th>Hobby</th>
+				<th>Sport,Travelling</th>
+			</tr>
+			<tr>
+				<th>Favorite</th>
+				<th>Durian</th>
+			</tr>
+
+		</tbody>
+	</table>
+		
+</div>
+</div>
+	</div>
+
+		
+		<div class="col-xs-offset-5 col-xs-1">
+			<a href="<?php echo site_url('home'); ?>"><button type="button" class="btn btn-success ">HOME</button></a>
+		</div>
+		<div class="col-xs-2">
+			<a href="<?php echo site_url('contact'); ?>"><button type="button" class="btn btn-success ">CONTACT</button></a>
+		</div>
+		</div>
+
+
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js" ></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
